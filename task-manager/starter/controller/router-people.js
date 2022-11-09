6d16defdeb0f7d09f621d1b8f9427c657b5c7aa8
hello-express <liuhaobin486@gmail.com>
2022-11-09 16:02:38 +0800

    init

---
+++ task-manager/starter/controller/router-people.js
@@ -0,0 +1,18 @@
+const getAllTasks=(req,res)=>{
+    res.send('all items')
+}
+const createTask=(req,res)=>{
+    res.json(req.body)
+}
+const getTask=(req,res)=>{
+    res.send('all items')
+}
+const updateTask=(req,res)=>{
+    res.send('all items')
+}
+const deleteTask=(req,res)=>{
+    res.send('all items')
+}
+module.exports ={
+    getAllTasks,createTask,getTask,updateTask,deleteTask
+}
