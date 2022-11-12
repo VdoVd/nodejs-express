6f23ceb665329d1aeb4c59f483a6561b77e5adef
hello-express <liuhaobin486@gmail.com>
2022-11-12 17:56:39 +0800

    curd api add complete

--- task-manager/starter/controller/router-people.js
+++ task-manager/starter/controller/router-people.js
@@ -16,14 +16,46 @@ const createTask=async (req,res)=>{
         res.status(500).json({msg: error})
     }
 }
-const getTask=(req,res)=>{
-     res.send('all items')
+const getTask=async (req,res)=>{
+    try {
+        const {id:taskID}=req.params
+        const task=await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch (err){
+        res.status(500).json({msg:err})
+    }
 }
-const updateTask=(req,res)=>{
-    res.send('all items')
+const updateTask=async (req,res)=>{
+    try {
+        const {id:taskID}=req.params
+
+
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if(!task){
+            return next(createCustomError(`No task with id : ${taskID}`, 404))
+        }
+        res.status(200).json({id:taskID,data:req.body})
+    }catch (err){
+
+    }
 }
-const deleteTask=(req,res)=>{
-    res.send('all items')
+const deleteTask=async (req,res)=>{
+    try {
+        const {id:taskId}=req.params
+        const task=await Task.findOneAndDelete({_id:taskId})
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskId}`})
+        }
+        res.status(200).json({task:null,status:'success'})
+    }catch (err){
+        res.status(500).json({msg:err})
+    }
 }
 module.exports ={
     getAllTasks,createTask,getTask,updateTask,deleteTask
