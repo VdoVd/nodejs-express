e0292b350b489879c4afd3380a10808eaecb8fac
hello-express <liuhaobin486@gmail.com>
2022-11-18 16:31:40 +0800

    add token

--- store-api/starter/populate.js
+++ store-api/starter/populate.js
@@ -6,15 +6,16 @@ const Product = require('./models/product')
 
 const jsonProducts = require('./products.json')
 
-
 const start =async () =>{
     try {
         await connectDB(process.env.MONGO_URL)
         await Product.deleteMany()
         await Product.create(jsonProducts)
-        console.log('success')
+        console.log('success!')
+        process.exit(0)
     }catch (err){
         console.log(err)
+        process.exit(1)
     }
 }
 
